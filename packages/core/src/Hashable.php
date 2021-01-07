6221c070a9abc2ce1ae8f2fe779b66462fcd37e1
Alex Brouwer <alexander@socialdeal.nl>
2021-01-07 12:29:35 +0100

    Added interface level docBlock to Hashable

--- packages/core/src/Hashable.php
+++ packages/core/src/Hashable.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace Par\Core;
 
+/**
+ * Hashable is an interface which allows objects to be used as keys.
+ *
+ * It's an alternative to spl_object_hash(), which determines an object's hash based on its handle: this means
+ * that two objects that are considered equal by an implicit definition would not be treated as equal because they are
+ * not the same instance.
+ */
 interface Hashable extends \Ds\Hashable
 {
 
