1504fec50a614e7135c91000faec9d91af471e2a
Alex Brouwer <alexander@socialdeal.nl>
2021-01-15 15:53:19 +0100

    Introduce trait for testing with enums

---
+++ packages/core/test/Unit/PHPUnit/EnumTestCaseTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ParTest\Core\Unit\PHPUnit;
+
+use Par\Core\Enum;
+use ReflectionClass;
+
+/**
+ * Trait containing a PHPUnit\Framework\TestCase::setUp implementation that will make sure enums work as expected
+ * between tests.
+ */
+trait EnumTestCaseTrait
+{
+    protected function setUp(): void
+    {
+        // Reset all static properties
+        $reflectionClass = new ReflectionClass(Enum::class);
+
+        $definitionCache = $reflectionClass->getProperty('definitionCache');
+        $definitionCache->setAccessible(true);
+        $definitionCache->setValue($definitionCache->getDefaultValue());
+
+        $instances = $reflectionClass->getProperty('instances');
+        $instances->setAccessible(true);
+        $instances->setValue($instances->getDefaultValue());
+
+        $allInstancesLoaded = $reflectionClass->getProperty('allInstancesLoaded');
+        $allInstancesLoaded->setAccessible(true);
+        $allInstancesLoaded->setValue($allInstancesLoaded->getDefaultValue());
+    }
+}
