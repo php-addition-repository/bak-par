a863f376d43fb64c04ed5b19b59df329123436ab
Alex Brouwer <alexander@socialdeal.nl>
2021-01-21 15:14:25 +0100

    Added YearMonth

---
+++ packages/time/src/YearMonth.php
@@ -0,0 +1,211 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Par\Time;
+
+use DateTimeInterface;
+use Par\Core\Hashable;
+use Stringable;
+
+/**
+ * A year-month in the ISO-8601 calendar system, such as 2007-12.
+ *
+ * YearMonth is an immutable date-time object that represents the combination of a year and month. Any field that can
+ * be derived from a year and month, such as quarter-of-year, can be obtained.
+ *
+ * This class does not store or represent a day, time or time-zone. For example, the value "October 2007" can be stored
+ * in a YearMonth.
+ *
+ * @psalm-immutable
+ */
+final class YearMonth implements Hashable, Stringable
+{
+    private Year $year;
+    private Month $month;
+
+    /**
+     * Obtains an instance of YearMonth from a year and month.
+     *
+     * @param int|Year  $year  The year to represent
+     * @param int|Month $month The month-of-year to represent
+     *
+     * @return self
+     * @psalm-mutation-free
+     */
+    public static function of(int|Year $year, int|Month $month): self
+    {
+        $year = is_int($year) ? Year::of($year) : $year;
+        $month = is_int($month) ? Month::of($month) : $month;
+
+        return new self($year, $month);
+    }
+
+    /**
+     * Obtains an instance of year-month from an implementation of the DateTimeInterface.
+     *
+     * @param DateTimeInterface $dateTime The datetime to convert
+     *
+     * @return self
+     * @psalm-mutation-free
+     */
+    public static function fromNative(DateTimeInterface $dateTime): self
+    {
+        /** @psalm-suppress ImpureMethodCall */
+        return self::of(Year::fromNative($dateTime), Month::fromNative($dateTime));
+    }
+
+    /**
+     * Obtains the current year-month from the system clock in the default time-zone.
+     *
+     * @return self
+     * @psalm-mutation-free
+     */
+    public static function now(): self
+    {
+        $now = Factory::now();
+
+        return self::fromNative($now);
+    }
+
+    /**
+     * Obtains an instance of YearMonth from a text string such as 2007-03.
+     *
+     * The string must represent a valid year-month. The format is Y-m.
+     *
+     * @param string $text The text to parse such as "2007-03"
+     *
+     * @return self The parsed year-month
+     */
+    public static function parse(string $text): self
+    {
+        Assert::regex($text, '/^-?\d{1,9}-\d{2}$/');
+
+        preg_match('/^(-?\d{1,9})-(\d{2})$/', $text, $matches);
+
+        $year = Year::of((int)$matches[1]);
+        $month = Month::of((int)$matches[2]);
+
+        return new self($year, $month);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @return int
+     */
+    public function hash(): int
+    {
+        // 12-3 -> 1203
+        return ($this->year->value() * 100) + $this->month->value();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function equals(mixed $other): bool
+    {
+        if ($other instanceof static) {
+            return $this->year->equals($other->year) && $this->month->equals($other->month);
+        }
+
+        return false;
+    }
+
+    /**
+     * Outputs this year-month as a String, such as 2007-12.
+     *
+     * The output will be in the format Y-m.
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return sprintf('%d-%02d', $this->year->value(), $this->month->value());
+    }
+
+    /**
+     * Outputs this year-month as a String, such as 2007-12.
+     *
+     * The output will be in the format Y-m.
+     *
+     * @return string
+     */
+    public function toString(): string
+    {
+        return (string)$this;
+    }
+
+    /**
+     * Gets the year field.
+     *
+     * @return Year
+     */
+    public function year(): Year
+    {
+        return $this->year;
+    }
+
+    /**
+     * Gets the primitive value of the year field.
+     *
+     * @return int
+     * @see Year::value()
+     */
+    public function yearValue(): int
+    {
+        return $this->year->value();
+    }
+
+    /**
+     * Gets the month-of-year field using the Month enum.
+     *
+     * @return Month
+     */
+    public function month(): Month
+    {
+        return $this->month;
+    }
+
+    /**
+     * Gets the month-of-year field from 1 to 12.
+     *
+     * @return int
+     * @see Month::value()
+     */
+    public function monthValue(): int
+    {
+        return $this->month->value();
+    }
+
+    /**
+     * Returns a copy of this YearMonth with the month-of-year altered.
+     *
+     * @param int|Month $month The month-of-year to set in the returned year-month, from 1 (January) to 12 (December)
+     *
+     * @return self A YearMonth based on this year-month with the requested month
+     */
+    public function withMonth(int|Month $month): self
+    {
+        return self::of($this->year, $month);
+    }
+
+    /**
+     * Returns a copy of this YearMonth with the year altered.
+     *
+     * @param int|Year $year The year to set in the returned year-month, from MIN_YEAR to MAX_YEAR
+     *
+     * @return self A YearMonth based on this year-month with the requested year
+     */
+    public function withYear(int|Year $year): self
+    {
+        return self::of($year, $this->month);
+    }
+
+    private function __construct(Year $year, Month $month)
+    {
+        $this->year = $year;
+        $this->month = $month;
+    }
+
+}
---
+++ packages/time/test/Unit/YearMonthTest.php
@@ -0,0 +1,189 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Par\TimeTest\Unit;
+
+use DateTime;
+use DateTimeImmutable;
+use DateTimeInterface;
+use Par\Core\PHPUnit\HashableAssertions;
+use Par\Time\Exception\InvalidArgumentException;
+use Par\Time\Factory;
+use Par\Time\Month;
+use Par\Time\PHPUnit\TimeTestCaseTrait;
+use Par\Time\Year;
+use Par\Time\YearMonth;
+use PHPUnit\Framework\TestCase;
+
+class YearMonthTest extends TestCase
+{
+    use TimeTestCaseTrait;
+    use HashableAssertions;
+
+    /**
+     * @test
+     */
+    public function itCanBeCreatedFromValue(): void
+    {
+        $expectedYear = 2011;
+        $expectedMonth = 7;
+        $monthDay = YearMonth::of($expectedYear, $expectedMonth);
+
+        self::assertSame($expectedYear, $monthDay->yearValue());
+        self::assertSame($expectedMonth, $monthDay->monthValue());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanBeTransformedToString(): void
+    {
+        self::assertSame(YearMonth::of(2020, 1)->toString(), '2020-01');
+    }
+
+    /**
+     * @test
+     */
+    public function itCanDetermineEqualityWithOther(): void
+    {
+        $yearMonth = YearMonth::of(2020, 1);
+        $otherYearMonth = YearMonth::of(2021, 2);
+
+        self::assertTrue($yearMonth->equals($yearMonth));
+        self::assertFalse($yearMonth->equals($otherYearMonth));
+        self::assertFalse($yearMonth->equals(null));
+    }
+
+    /**
+     * @test
+     */
+    public function itCanUpdateMonth(): void
+    {
+        $yearMonth = YearMonth::of(2020, 1);
+
+        self::assertHashEquals(
+            Month::of(4),
+            $yearMonth->withMonth(Month::of(4))->month()
+        );
+        self::assertHashEquals($yearMonth->year(), $yearMonth->withMonth(Month::of(4))->year());
+        self::assertHashNotEquals($yearMonth, $yearMonth->withMonth(Month::of(4)));
+
+        self::assertHashEquals(Month::of(4), $yearMonth->withMonth(4)->month());
+        self::assertHashEquals($yearMonth->year(), $yearMonth->withMonth(4)->year());
+        self::assertHashNotEquals($yearMonth, $yearMonth->withMonth(4));
+    }
+
+    /**
+     * @test
+     */
+    public function itCanUpdateYear(): void
+    {
+        $yearMonth = YearMonth::of(2020, 1);
+
+        self::assertHashEquals(
+            Year::of(1995),
+            $yearMonth->withYear(Year::of(1995))->year()
+        );
+        self::assertHashEquals($yearMonth->month(), $yearMonth->withYear(Year::of(1995))->month());
+        self::assertHashNotEquals($yearMonth, $yearMonth->withYear(Year::of(1995)));
+
+        self::assertHashEquals(Year::of(1995), $yearMonth->withYear(1995)->year());
+        self::assertHashEquals($yearMonth->month(), $yearMonth->withYear(1995)->month());
+        self::assertHashNotEquals($yearMonth, $yearMonth->withYear(1995));
+    }
+
+    /**
+     * @test
+     */
+    public function itIsHashable(): void
+    {
+        self::assertSame(200103, YearMonth::of(2001, 3)->hash());
+    }
+
+    /**
+     * @return array<string, array{YearMonth, DateTimeInterface}>
+     */
+    public function provideNative(): array
+    {
+        return [
+            DateTime::class => [
+                YearMonth::of(2021, 1),
+                DateTime::createFromImmutable(Factory::createDate(2021, 1, 14)),
+            ],
+            DateTimeImmutable::class => [
+                YearMonth::of(2019, 7),
+                Factory::createDate(2019, 7, 20),
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider provideNative
+     *
+     * @param YearMonth         $expected
+     * @param DateTimeInterface $native
+     *
+     * @return void
+     */
+    public function itCanBeCreatedFromNativeDateTime(YearMonth $expected, DateTimeInterface $native): void
+    {
+        self::assertHashEquals($expected, YearMonth::fromNative($native));
+    }
+
+    /**
+     * @test
+     */
+    public function itCanBeCreatedForCurrentYear(): void
+    {
+        $this->wrapWithTestNow(
+            static function () {
+                $now = Factory::now();
+
+                $current = YearMonth::now();
+
+                self::assertSameYear($now, $current->yearValue());
+                self::assertSameMonth($now, $current->monthValue());
+            }
+        );
+    }
+
+    /**
+     * @return array<string, array{string, YearMonth}>
+     */
+    public function provideStrings(): array
+    {
+        return [
+            'month < 10' => ['2001-02', YearMonth::of(2001, 2)],
+            'month > 10' => ['2001-12', YearMonth::of(2001, 12)],
+            'large year' => ['999999-01', YearMonth::of(999999, 1)],
+            'small year' => ['1-01', YearMonth::of(1, 1)],
+            'zero year' => ['0-01', YearMonth::of(0, 1)],
+            'negative year' => ['-2-01', YearMonth::of(-2, 1)],
+        ];
+    }
+
+    /**
+     * @dataProvider provideStrings
+     *
+     * @param string    $text
+     * @param YearMonth $expected
+     *
+     * @test
+     */
+    public function itCanBeCreatedFromString(string $text, YearMonth $expected): void
+    {
+        self::assertHashEquals($expected, YearMonth::parse($text));
+    }
+
+    /**
+     * @test
+     */
+    public function ItWillThrowExceptionWhenCreatingFromInvalidString(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        YearMonth::parse('The year 2000');
+    }
+}
