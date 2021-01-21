ffe160bc446025140686c2875ffe61ec3d2b2b23
Alex Brouwer <alexander@socialdeal.nl>
2021-01-21 12:45:19 +0100

    Add Par\Time\Year

---
+++ packages/time/src/Year.php
@@ -0,0 +1,164 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Par\Time;
+
+use DateTimeInterface;
+use Par\Core\Hashable;
+use Par\Time\Exception\InvalidArgumentException;
+use Stringable;
+
+/**
+ * A year in the ISO-8601 calendar system, such as 2007.
+ *
+ * @psalm-immutable
+ */
+final class Year implements Hashable, Stringable
+{
+    public const MIN_VALUE = -999999999;
+    public const MAX_VALUE = 999999999;
+
+    private const DATE_FORMAT = 'Y';
+
+    private int $value;
+
+    /**
+     * Obtains the current year from the system clock in the default time-zone.
+     *
+     * @return self
+     * @psalm-mutation-free
+     */
+    public static function now(): self
+    {
+        $now = Factory::now();
+
+        return self::fromNative($now);
+    }
+
+    /**
+     * Obtains an instance of DayOfWeek from an implementation of the DateTimeInterface.
+     *
+     * @param DateTimeInterface $dateTime The datetime to convert
+     *
+     * @return self
+     * @psalm-mutation-free
+     */
+    public static function fromNative(DateTimeInterface $dateTime): self
+    {
+        /** @psalm-suppress ImpureMethodCall */
+        return self::of((int)$dateTime->format(self::DATE_FORMAT));
+    }
+
+    /**
+     * Obtains an instance of Year.
+     *
+     * @param int $year The year to represent
+     *
+     * @return self
+     * @psalm-mutation-free
+     */
+    public static function of(int $year): self
+    {
+        return new self($year);
+    }
+
+    /**
+     * Obtains an instance of Year from a text string such as 2007.
+     *
+     * @param string $text The text to parse
+     *
+     * @return self
+     * @throws InvalidArgumentException If text could not be parsed or value is outside of range
+     * @psalm-mutation-free
+     */
+    public static function parse(string $text): self
+    {
+        Assert::regex($text, '/^[-+]?\d{1,}$/');
+
+        return new self((int)$text);
+    }
+
+    /**
+     * Checks if the year is a leap year, according to the ISO calendar system rules.
+     *
+     * @param int $year
+     *
+     * @return bool
+     * @psalm-mutation-free
+     */
+    public static function isLeapYear(int $year): bool
+    {
+        if ($year === 0) {
+            return false;
+        }
+        $dt = Factory::createDate($year);
+
+        return (int)$dt->format('L') === 1;
+    }
+
+    public function equals(mixed $other): bool
+    {
+        if ($other instanceof static) {
+            return $this->hash() === $other->hash();
+        }
+
+        return false;
+    }
+
+    public function hash(): int
+    {
+        return $this->value;
+    }
+
+    /**
+     * Gets the year value.
+     *
+     * @return int
+     */
+    public function value(): int
+    {
+        return $this->value;
+    }
+
+    /**
+     * Gets the length of this year in days.
+     *
+     * @return int 365 or 366
+     */
+    public function length(): int
+    {
+        return $this->isLeap() ? 366 : 365;
+    }
+
+    /**
+     * Checks if the year is a leap year, according to the ISO calendar system rules.
+     *
+     * @return bool
+     * @see Year::isLeapYear
+     */
+    public function isLeap(): bool
+    {
+        return static::isLeapYear($this->value);
+    }
+
+    public function toString(): string
+    {
+        return (string)$this;
+    }
+
+    public function __toString(): string
+    {
+        return (string)$this->value;
+    }
+
+    /**
+     * @throws InvalidArgumentException If year is outside of range
+     */
+    private function __construct(int $year)
+    {
+        Assert::range($year, self::MIN_VALUE, self::MAX_VALUE);
+
+        $this->value = $year;
+    }
+}
---
+++ packages/time/test/Unit/YearTest.php
@@ -0,0 +1,199 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ParTest\Time\Unit;
+
+use DateTime;
+use DateTimeImmutable;
+use DateTimeInterface;
+use InvalidArgumentException;
+use Par\Core\PHPUnit\HashableAssertions;
+use Par\Time\Factory;
+use Par\Time\PHPUnit\TimeTestCaseTrait;
+use Par\Time\Year;
+use PHPUnit\Framework\TestCase;
+
+class YearTest extends TestCase
+{
+    use TimeTestCaseTrait;
+    use HashableAssertions;
+
+    /**
+     * @return array<string, array{string, Year}>
+     */
+    public function provideStrings(): array
+    {
+        return [
+            'Numeric string' => ['2000', Year::of(2000)],
+            'Negative numeric string' => ['-2000', Year::of(-2000)],
+            'Positive numeric string' => ['+2000', Year::of(2000)],
+            'Numeric string with zeros' => ['0000', Year::of(0)],
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function itCanBeCreatedFromValue(): void
+    {
+        $expected = 2019;
+        $year = Year::of($expected);
+
+        self::assertSame($expected, $year->value());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanBeCreatedForCurrentYear(): void
+    {
+        $this->wrapWithTestNow(
+            static function () {
+                $now = Factory::now();
+
+                $currentYear = Year::now();
+
+                self::assertSameYear($now, $currentYear->value());
+            }
+        );
+    }
+
+    /**
+     * @dataProvider provideStrings
+     *
+     * @param string $text
+     * @param Year   $expectedYear
+     *
+     * @test
+     */
+    public function itCanBeCreatedFromString(string $text, Year $expectedYear): void
+    {
+        self::assertHashEquals($expectedYear, Year::parse($text));
+    }
+
+    /**
+     * @return array<string, array{Year, DateTimeInterface}>
+     */
+    public function provideNative(): array
+    {
+        return [
+            DateTime::class => [
+                Year::of(2021),
+                DateTime::createFromImmutable(Factory::createDate(2021, 1, 14)),
+            ],
+            DateTimeImmutable::class => [
+                Year::of(2019),
+                Factory::createDate(2019, 7, 20),
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider provideNative
+     *
+     * @param Year              $expected
+     * @param DateTimeInterface $native
+     *
+     * @return void
+     */
+    public function itCanBeCreatedFromNativeDateTime(Year $expected, DateTimeInterface $native): void
+    {
+        self::assertHashEquals($expected, Year::fromNative($native));
+    }
+
+    /**
+     * @test
+     */
+    public function itCanDetermineEqualityWithOther(): void
+    {
+        $year = Year::of(2001);
+        $otherYear = Year::of(2012);
+
+        self::assertTrue($year->equals($year));
+        self::assertFalse($year->equals($otherYear));
+        self::assertFalse($year->equals(null));
+    }
+
+    /**
+     * @test
+     */
+    public function itWillReturnValue(): void
+    {
+        self::assertSame(2001, Year::of(2001)->value());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanDetermineIfYearIsLeapYear(): void
+    {
+        self::assertTrue(Year::of(1904)->isLeap()); // divisible by 4
+        self::assertTrue(Year::of(2000)->isLeap()); // divisible by 400
+
+        self::assertFalse(Year::of(1900)->isLeap()); // divisible by 100
+        self::assertFalse(Year::of(1901)->isLeap()); // not divisible by 4
+    }
+
+    /**
+     * @test
+     */
+    public function itCanProvideTheLengthInDays(): void
+    {
+        self::assertSame(365, Year::of(1995)->length());
+        self::assertSame(366, Year::of(2000)->length());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanBeTransformedToString(): void
+    {
+        self::assertSame(Year::of(2012)->toString(), '2012');
+    }
+
+    /**
+     * @test
+     */
+    public function ItWillThrowExceptionWhenCreatingFromInvalidString(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Year::parse('The year 2000');
+    }
+
+    /**
+     * @test
+     */
+    public function itWillThrowExceptionWhenCreatingFromIntegerGreaterThanMax(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Year::of(Year::MAX_VALUE + 1);
+    }
+
+    /**
+     * @test
+     */
+    public function itWillThrowExceptionWhenCreatingFromIntegerLessThanMin(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Year::of(Year::MIN_VALUE - 1);
+    }
+
+    /**
+     * @test
+     */
+    public function itCanDetermineIfIntegerIsLeapYear(): void
+    {
+        self::assertTrue(Year::isLeapYear(1904)); // divisible by 4
+        self::assertTrue(Year::isLeapYear(2000)); // divisible by 400
+
+        self::assertFalse(Year::isLeapYear(1900)); // divisible by 100
+        self::assertFalse(Year::isLeapYear(1901)); // not divisible by 4
+
+        self::assertFalse(Year::isLeapYear(0)); // not divisible at all
+    }
+}
